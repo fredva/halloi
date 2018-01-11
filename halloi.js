83c274047991f5b582d2311269c8ba8030c17174
Fredrik V. Mørken <fredrik@valdmanis.com>
2018-01-11 20:04:16 +0100

    ???

--- halloi.js
+++ halloi.js
@@ -7,12 +7,32 @@ var halloi =
 "18.7;halloi;xl\n" +
 "19.2;shout out to kmj\n" +
 "20.7;han var en top notch ulær i fra back in the day\n" +
-"23;ser'n fortsatt rundt i gata\n" +
+"23.2;ser'n fortsatt rundt i gata\n" +
 "24.5;rapping away\n" +
-"25.7;???\n" +
-"27.8;mens jeg lever for noen kroner og ei beta\n" +
+"25.7;tjenær på no late night smack og no haze\n" +
+"27.8;mens jeg lever for noen kroner og en beta\n" +
 "30.4;men det ække no å laga no\n" +
 "31.5;halloi;xl\n" + 
-"32;for detta" 
+"32;for detta;\n" +
+"32.8;stilen er smek;\n" +
+"34.2;tenner en bong;\n" +
+"35.5;kjøper hink av en fyr med pannebånd;\n" +
+"37.9;hakke tall på hvor mange munner jeg har metta;\n" +
+"39.9;men det ække no å laga no;\n" +
+"41;halloi;xl\n" +
+"41.5;for detta;\n" +
+"42.1;vi svetta\n" +
+"42.9;vi tok av;\n" +
+"43.4;vi letta;\n" +
+"44.1;satt ???;\n" +
+"44.6;til metal;\n" +
+"45.1;på sletta;\n" +
+"45.7;fikk noia\n" +
+"46.4;fikk hetta\n" +
+
+
+"174;det ække no å laga no halloi for detta\n" +
+"175.8;\n" +
+"176.3;det ække no å laga no halloi for detta"
 ;
 
