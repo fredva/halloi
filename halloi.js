9133a981ca7ca75ad54f9a4d29ab2a8f58ae5d45
Fredrik V. Mørken <fredrik@valdmanis.com>
2018-01-21 12:06:44 +0100

    MEN DET ÆKKE NO Å LAGA NO HALLOI FOR DETTA

--- halloi.js
+++ halloi.js
@@ -1,22 +1,31 @@
 var halloi =
-"0;\n" +
+"0;🔊\n" +
 "11;halloi;xs\n" +
 "13.3;halloi;s\n" +
 "15.6;halloi\n" +
 "17.9;halloi;l\n" +
 "18.7;halloi;xl\n" +
-"19.2;shout out to kmj\n" +
-"20.7;han var en top notch ulær i fra back in the day\n" +
+"19.2;shout out to\n" +
+"19.9;k;xl\n" +
+"20.1;m;xl\n" +
+"20.4;j;xl\n" +
+"20.7;han var en top notch ulær i fra\n" +
+"22.2;back;l\n" +
+"22.4;in;l\n" +
+"22.6;the;l\n" +
+"22.8;days;l\n" +
 "23.2;ser'n fortsatt rundt i gata\n" +
-"24.5;rapping away\n" +
+"24.5;rapping away;l\n" +
 "25.7;tjenær på no late night smack og no haze\n" +
 "27.8;mens jeg lever for noen kroner og en beta\n" +
+"29.9;halloi;s\n" +
 "30.4;men det ække no å laga no\n" +
 "31.5;halloi;xl\n" + 
 "32;for detta;\n" +
 "32.8;stilen er smek;\n" +
 "34.2;tenner en bong;\n" +
 "35.5;kjøper hink av en fyr med pannebånd;\n" +
+"37.3;halloi;s\n" +
 "37.9;hakke tall på hvor mange munner jeg har metta;\n" +
 "39.9;men det ække no å laga no;\n" +
 "41;halloi;xl\n" +
@@ -32,7 +41,7 @@ var halloi =
 "46.9;men det ække no å laga no;\n" +
 "48;halloi;xl\n" +
 "48.5;for detta;\n" +
-"49.2;det ække no å laga no halloi 🎵;\n" +
+"49.2;det ække no å laga no halloi;\n" +
 "52;halloi;xl\n" +
 "52.8;partyboys får hetta;\n" +
 "53.9;men det ække no å laga no;\n" +
@@ -55,19 +64,19 @@ var halloi =
 "72;for detta;\n" +
 "72.9;det ække no å laga no;\n" +
 "73.9;halloi;xl\n" +
-"74.7;jeg sveveri opp opp opp opp opp;\n" +
+"74.7;jeg svever jo opp opp opp opp opp;\n" +
 "76.7;kan du følge meg ned;\n" +
 "78;følge meg ned;\n" +
 "79.3;jeg ruller en rev;\n" +
 "80.6;ringen er smek;\n" +
-"81.7;jeg sveveri opp;\n" +
+"81.7;jeg svever jo opp;\n" +
 "82.5;kan du følge meg ned;\n" +
-"84.1;jeg sveveri opp opp opp opp opp;\n" +
+"84.1;jeg svever jo opp opp opp opp opp;\n" +
 "86.2;kan du følge meg ned;\n" +
 "87.5;følge meg ned;\n" +
 "88.8;jeg ruller en rev;\n" +
 "89.8;ringen er smek;\n" +
-"90.9;jeg sveveri opp;\n" +
+"90.9;jeg svever jo opp;\n" +
 "92;kan du følge følge følge meg ned;\n" +
 "94;påsa med molly;\n" +
 "95.1;få som betaler;\n" +
@@ -79,10 +88,49 @@ var halloi =
 "102.3;sme;l\n" +
 "102.9;ke;l\n" +
 "103.5;reven;xl\n" +
-
-
-"174;det ække no å laga no halloi for detta\n" +
-"175.8;\n" +
-"176.3;det ække no å laga no halloi for detta"
+"103.9;det er meg ja!\n" +
+"104.5;unge tommy teia\n" +
+"105.7;jeg bor i samma byen som han der\n" +
+"106.9;baneheia greia;l\n" +
+"107.9;noen kaller det miami\n" +
+"109;noen kaller det marbella\n" +
+"110.2;hele byen er full av horer\n" +
+"111.5;jeg bare kaller det pattaya\n" +
+"112.8;røyker masse hink til jeg blir høy som himalaya\n" +
+"115.1;jeg hater politi jeg har en apekatt i bleia\n" +
+"117.5;kan sørge for at du må plukke plommer i madeira\n" +
+"120;hva med deg ja hva med deg ja\n" +
+"122;jeg kjenner snobber som jobber\n" +
+"123.4;som håver inn flus!\n" +
+"124.4;og folk som ikke jobber\n" +
+"125.7;som jobber med rus!\n" +
+"126.8;det er mange joggedresser\n" +
+"128.1;men ingen av dem som jogger\n" +
+"129.2;og folk som går på nav\n" +
+"130.3;det er ingen av dem som jobber!\n" +
+"131.7;vi bobber, weaver, og lavær halloi\n" +
+"133.8;la meg høre alle rope halloi!\n" +
+"135.8;halloi!;xl\n" +
+"136.3;vi bare bobber, weaver, og lavær halloi\n" +
+"138.4;la meg høre alle rope halloi-oi-oi\n" +
+"141;det ække no å laga no halloi;xs\n" +
+"143.4;det ække no å laga no halloi (nei);s\n" +
+"145.7;det ække no å laga no halloi for detta\n" +
+"148;det ække no å laga no halloi;l\n" +
+"149.9;jeg svever jo opp opp opp opp opp;\n" +
+"152;kan du følge meg ned;\n" +
+"153.4;følge meg ned;\n" +
+"154.5;jeg ruller en rev;\n" +
+"155.7;ringen er smek;\n" +
+"156.9;jeg svever jo opp;\n" +
+"157.9;kan du følge meg ned;\n" +
+"159.3;jeg svever jo opp opp opp opp opp;\n" +
+"161.4;kan du følge meg ned;\n" +
+"162.8;følge meg ned;\n" +
+"163.9;jeg ruller en rev;\n" +
+"165.1;ringen er smek;\n" +
+"166.3;jeg svever jo opp;\n" +
+"167.2;kan du følge følge følge meg ned;\n" + 
+"172;🔊"
 ;
 
